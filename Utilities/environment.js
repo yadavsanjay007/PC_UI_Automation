7903a4e6509527fd2907012b102e67648a0b9fff
yadavsanjay007 <sanjaykumar@frugaltestingin.com>
2026-02-12 11:20:14 +0530

    added yml file

--- Utilities/environment.js
+++ Utilities/environment.js
@@ -1,4 +1,4 @@
 export const environments = {
-    baseURL: 'https://partscheck.com.au/global/index.php',
-    dashboardURL: 'https://v2-next.partscheck.com.au/appV2/get-price.php'
+    baseURL: 'https://dev.partscheck.com.au/global/index.php',
+    dashboardURL: 'https://v2.dev.partscheck.com.au/appV2/get-price.php'
 };
